75b8614153057ed824856e4e7a82619755fe3ec0
syemfai <syemfai@gmail.com>
2024-05-13 11:49:38 +0800

    Delete plural files

--- Clients.java
+++
@@ -1,9 +0,0 @@
-import java.util.*; 
-
-public class Clients {
-  public HashMap<String, Client> clients;
-
-  public Clients(String clientsCSV) {
-    //implement CSV
-  }
-}
--- Instruments.java
+++
@@ -1,9 +0,0 @@
-import java.util.*; 
-
-public class Instruments {
-  public HashMap<String, Instrument> instruments;
-
-  public Instruments(String instrumentsCSV) {
-    //implement
-  }
-}
