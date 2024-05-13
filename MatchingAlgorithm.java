8435b18e1fe0034c6aaf9cd71a7ad03f63f7e414
raynor-koh <raynorkoh01@outlook.com>
2024-05-13 16:31:58 +0800

    Try openPriceMatch

--- MatchingAlgorithm.java
+++ MatchingAlgorithm.java
@@ -208,6 +208,14 @@ public class MatchingAlgorithm {
     return true;
   }
 
+  public void openAuctionMatch(Instrument instrument) {
+    double openPrice = findOpenPrice(instrument);
+    List<Order> auction = Stream.of((Order[]) Order.orderHashSet.toArray())
+            .filter(x -> x.instrument.equals(instrument))
+            .filter(x -> x.time.compareTo(LocalTime.of(9,30,0)) < 0)
+            .collect(Collectors.toList());
+  }
+
   public static void main(String[] args) {
     MatchingAlgorithm x = new MatchingAlgorithm();
   }
