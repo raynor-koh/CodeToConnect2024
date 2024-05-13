3bcc34b2bb910a38385072ad134278c22caf43d9
syemfai <syemfai@gmail.com>
2024-05-13 09:48:53 +0800

    Updated Matching Algorithm

--- MatchingAlgorithm.java
+++ MatchingAlgorithm.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList; 
+
 public class MatchingAlgorithm {
   public String type; //Auction or Continuous
+  public ArrayList<Order> orders;
+  public HashMap<>;
 
-  public MatchingAlgorithm(String type) {
-    this.type = type;
+  public MatchingAlgorithm(String instrumentsCSV, String clientsCSV, String ordersCSV) {
+    this.allInstruments = Instrument(instrumentsCSV);
+    this.allClients = Client(clientsCSV);
+    this.addOrders = Order(ordersCSV);
   }
 
-  //test
-
+  
+  
 }
